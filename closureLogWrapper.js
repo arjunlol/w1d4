55ced1149ae885586abec82336e1b103be4c9bd7
Arjun Lall <lallarjun@gmail.com>
2017-05-04 19:16:14 +0000

    refactored closure logging wrapper to use apply

--- closureLogWrapper.js
+++ closureLogWrapper.js
@@ -1,13 +1,17 @@
 var wrapLog = function (callback, name) {
   /* your code here */
-  return function (a,b) {
-    var total = sum(a,b);
-    console.log(name + "("+ a+ "," + b + ") =>" + total);
+  return function () {
+    result = callback.apply(null,arguments)
+    params = [].join.call(arguments, ',')
+    console.log(`${name}({params}) = > ${result}`)
   }
 };
 
 var sum = function (a,b) { return a + b; };
 var logSum = wrapLog(sum, "sum");
+
+
+
 
 logSum(5,3); // sum(5,3) => 8
 logSum(3,2); // sum(3,2) => 5
