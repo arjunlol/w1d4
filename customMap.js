23448dc0572c22cbdab8be72e2ad5c48f4ab94ec
Arjun Lall <lallarjun@gmail.com>
2017-05-04 16:49:50 +0000

    added the map function -returns array filled with callback function values

--- customMap.js
+++ customMap.js
@@ -1,8 +1,20 @@
 var words = ["ground", "control", "to", "major", "tom"];
 
-map(words, function(word) {
+function map (arr, func){
+  var counter = 0;
+  var result = [];
+  for (var value of arr){
+    result[counter] = func(value);
+    counter++;
+  }
+  return result;
+};
+
+
+var output = map(words, function(word) {
   return word.length;
 });
 
+console.log(output);
 
 //output [6, 7, 2, 5, 3]
