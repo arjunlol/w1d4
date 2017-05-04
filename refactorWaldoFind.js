ef8bd3f3e620b5ecc11a882a6ac99dbc0e11505e
Arjun Lall <lallarjun@gmail.com>
2017-05-04 15:02:38 +0000

    refactored to use foreach method

--- refactorWaldoFind.js
+++ refactorWaldoFind.js
@@ -1,10 +1,9 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
-    }
-  }
+  arr.forEach(function(currentValue, index){
+    if(currentValue === "Waldo")
+      found(index);
+  });
 }
 
 function actionWhenFound(index) {
